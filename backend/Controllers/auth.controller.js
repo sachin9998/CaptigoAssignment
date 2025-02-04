b0f1d7c1b0c6f2d0c18aaf54831ec757b9f6f971
Sachin Alam <sachinalam4216@gmail.com>
2025-02-04 15:12:22 +0530

    new change

--- backend/Controllers/auth.controller.js
+++ backend/Controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { User } from "../Models/User.js";
+import { User } from "../models/User.js";
+// import { User } from "../Models/User.js";
 
 export const signup = async (req, res) => {
   try {
