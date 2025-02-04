9afc0f48dbf5d094504dc4d314033501e66c5b5a
Sachin Alam <sachinalam4216@gmail.com>
2025-02-04 15:17:18 +0530

    edit 56

--- backend/Controllers/course.controller.js
+++ backend/Controllers/course.controller.js
@@ -1,5 +1,5 @@
 import Course from "../Models/Course.js";
-import { User } from "../models/User.js";
+import { User } from "../Models/User.js";
 
 export const createCourse = async (req, res) => {
   try {
